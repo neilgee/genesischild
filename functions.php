c9036c31dee01eaeb102a441a0f7791dacbe40a0
Neil Gee <neil@coolestguyplanettech.com>
2014-03-09 12:27:58 +1100

    initial commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+// Start the engine the other way
+
+add_action('genesis_setup','genesischild_theme_setup', 11); 
+function genesischild_theme_setup() { 
+	
+	add_theme_support( 'html5' );
+	add_theme_support( 'genesis-responsive-viewport' );
+	add_theme_support( 'genesis-footer-widgets', 3 );
+	
+}
