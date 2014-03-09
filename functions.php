cdc22384a6f76a325c5876df276baa7af1a19129
Neil Gee <neil@coolestguyplanettech.com>
2014-03-09 15:34:01 +1100

    withdraw html5 support

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 add_action('genesis_setup','genesischild_theme_setup'); 
 function genesischild_theme_setup() { 
 	
-	add_theme_support( 'html5' );
+
 	add_theme_support( 'genesis-responsive-viewport' );
 	add_theme_support( 'genesis-footer-widgets', 3 );
 	
