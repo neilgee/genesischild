03cd18794bd32c343eec849006e9e695658561f0
Neil Gee <neil@coolestguidesontheplanet.com>
2014-03-17 11:44:47 +1100

    Add in additional javascript & CSS for fonts and menu

--- functions.php
+++ functions.php
@@ -13,3 +13,17 @@ function genesischild_theme_setup() {
 //Functions Go Here
 
 //Script-Tac-ulous -> All the Scripts Registered and Enqueued
+function genesischild_scripts_styles(){
+	//thescript
+	wp_register_script ('placeholder', get_stylesheet_directory_uri() . '/js/placeholder.js', array( 'jquery' ),'1',true);
+	wp_register_script ('responsive', get_stylesheet_directory_uri() . '/js/responsive.js', array( 'jquery' ),'1',true);
+	wp_register_style ('googlefonts','http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,700,300','', '2', 'all');
+	wp_register_style ('fontawesome','//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css','', '4.0.3', 'all');
+
+
+
+	wp_enqueue_script('responsive');
+	wp_enqueue_script('placeholder');
+	wp_enqueue_style('googlefonts');
+	wp_enqueue_style('fontawesome');
+}
--- readme.txt
+++ readme.txt
@@ -1,1 +1,18 @@
 This is a basic started template for Genesis Child Theme
+
+17th March
+Added into functions.php scripts and styles register & enqueue
+
+Google Fonts
+FontAwesome
+Placeholder.js
+Responsive Menu
+
+Added in JS folder
+
+placeholder.js
+responsive.js
+
+Added in style.css
+
+responsive menu CSS
