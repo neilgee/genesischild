ac86940bbc259fcfc957c7b216539e8ec54ebce3
Neil Gee <neil@coolestguidesontheplanet.com>
2014-03-21 16:46:56 +1100

    Removed Lato with Cabin Google Font

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ function genesischild_scripts_styles(){
 	//thescript
 	wp_register_script ('placeholder', get_stylesheet_directory_uri() . '/js/placeholder.js', array( 'jquery' ),'1',true);
 	wp_register_script ('responsive', get_stylesheet_directory_uri() . '/js/responsive.js', array( 'jquery' ),'1',true);
-	wp_register_style ('googlefonts','http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,700,300','', '2', 'all');
+	wp_register_style ('googlefonts','http://fonts.googleapis.com/css?family=Cabin:400,500,600,700,400italic,500italic,600italic,700italic','', '2', 'all');
 	wp_register_style ('fontawesome','//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css','', '4.0.3', 'all');
 
 
