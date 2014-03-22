ddd67e000cc322e05492bbcbbb2a67375c004dd8
Neil Gee <neil@coolestguidesontheplanet.com>
2014-03-22 13:40:02 +1100

    Added in Widgetsied Footer

--- functions.php
+++ functions.php
@@ -12,7 +12,9 @@ function genesischild_theme_setup() {
 	//add_action('genesis_before', 'likebox_facebook_script');
 	remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
 	add_action( 'wp_enqueue_scripts', 'genesis_enqueue_main_stylesheet', 999 );
-	
+	remove_action( 'genesis_footer', 'genesis_do_footer' );
+	add_action( 'widgets_init', 'genesischild_extra_widgets' );	
+	add_action('genesis_footer','genesischild_footer_widget');			
 }
 
 //Functions Go Here
@@ -25,15 +27,33 @@ function genesischild_scripts_styles(){
 	wp_register_style ('googlefonts','http://fonts.googleapis.com/css?family=Cabin:400,500,600,700,400italic,500italic,600italic,700italic','', '2', 'all');
 	wp_register_style ('fontawesome','//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css','', '4.0.3', 'all');
 
-
-
 	wp_enqueue_script('responsive');
 	wp_enqueue_script('placeholder');
 	wp_enqueue_style('googlefonts');
 	wp_enqueue_style('fontawesome');
 }
 
-//Function for Facebook HTML5 Script needs to go after body - escape all inner double quotes
-//function likebox_facebook_script () {
-//	echo "";
-//}
+//Add in new Widget areas
+function genesischild_extra_widgets() {	
+	genesis_register_sidebar( array(
+	'id'          => 'footercontent',
+	'name'        => __( 'Footer', 'genesischild' ),
+	'description' => __( 'This is the general footer area', 'genesischild' ),
+	'before_widget' => '<div class="footercontent">',
+    'after_widget' => '</div>',
+	) );
+}
+
+	
+//Position the Footer Area
+function genesischild_footer_widget() {
+    genesis_widget_area ('footercontent', array(
+        'before' => '<div class="footercontainer">',
+        'after' => '</div>',));
+}
+
+
+/*Function for Facebook HTML5 Script needs to go after body - escape all inner double quotes
+function likebox_facebook_script () {
+echo "";
+}*/
