a7d6dd4e8db309e5c86f18c82c856bd61e1c1318
Neil Gee <neil@coolestguidesontheplanet.com>
2014-04-06 14:09:48 +1000

    New Widget Areas

--- functions.php
+++ functions.php
@@ -4,22 +4,37 @@
 add_action('genesis_setup','genesischild_theme_setup'); 
 function genesischild_theme_setup() { 
 	
-	add_theme_support( 'html5' );
-	add_theme_support( 'genesis-responsive-viewport' );
-	add_theme_support( 'genesis-footer-widgets', 3 );
-	add_theme_support( 'custom-background' );
-	//add_theme_support( 'genesis-connect-woocommerce' );
+	add_theme_support('html5');
+	add_theme_support('genesis-responsive-viewport');
+	add_theme_support('genesis-footer-widgets', 3);
+	add_theme_support('custom-background');
+	//add_theme_support('genesis-connect-woocommerce');
+	
+	remove_action('genesis_meta', 'genesis_load_stylesheet' );
+	remove_action('genesis_footer', 'genesis_do_footer' );
+	
+
+	
+	add_action('wp_enqueue_scripts', 'genesis_enqueue_main_stylesheet', 998);
+	add_action('wp_enqueue_scripts', 'genesischild_scripts_styles',999);
+	add_action('widgets_init', 'genesischild_extra_widgets');	
+	add_action('genesis_before_loop','genesischild_beforecontent_widget');
+	add_action('genesis_after_loop','genesischild_aftercontent_widget');
+	add_action('genesis_before_footer','genesischild_footerwidgetheader',5);
+	add_action('genesis_footer','genesischild_footer_widget');
+	add_action('genesis_footer','genesischild_postfooter_widget');		
+	add_action('genesis_before_header','genesischild_preheader_widget');
+	add_action('genesis_after_header','genesischild_optin_widget');
 	//add_action('genesis_before', 'likebox_facebook_script');
-	remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
-	add_action( 'wp_enqueue_scripts', 'genesis_enqueue_main_stylesheet', 998);
-	add_action( 'wp_enqueue_scripts', 'genesischild_scripts_styles',999);	
-	remove_action( 'genesis_footer', 'genesis_do_footer' );
-	add_action( 'widgets_init', 'genesischild_extra_widgets' );	
-	add_action('genesis_footer','genesischild_footer_widget');	
+
+	
 	add_filter('widget_text', 'do_shortcode');	
 	add_filter('widget_text','execute_php_widgets',10);	
 	add_filter( 'excerpt_more', 'genesischild_read_more_link' );
+
+		
 }
+
 
 //Child Theme Functions Go Here
 
@@ -30,9 +45,9 @@ function genesischild_scripts_styles(){
 	wp_register_script ('responsive', get_stylesheet_directory_uri() . '/js/responsive.js', array( 'jquery' ),'1',true);
 	wp_register_style ('googlefonts','http://fonts.googleapis.com/css?family=Cabin:400,500,600,700,400italic,500italic,600italic,700italic','', '2', 'all');
 	wp_register_style ('fontawesome','//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css','', '4.0.3', 'all');
-	wp_register_style( 'ie8', get_stylesheet_directory_uri() . '/ie8.css'  );
+	wp_register_style( 'ie8', get_stylesheet_directory_uri() . '/ie8.css'  );//target IE8 and Lower
 	$GLOBALS['wp_styles']->add_data( 'ie8', 'conditional', '/css/lte IE 8' );
-	wp_register_style( 'ieall', get_stylesheet_directory_uri() . '/css/ieall.css'  );
+	wp_register_style( 'ieall', get_stylesheet_directory_uri() . '/css/ieall.css'  );//target IE9 and lower
 	$GLOBALS['wp_styles']->add_data( 'ieall', 'conditional', 'IE' );
 
 	wp_enqueue_script('responsive');
@@ -47,14 +62,100 @@ function genesischild_scripts_styles(){
 //Add in new Widget areas
 function genesischild_extra_widgets() {	
 	genesis_register_sidebar( array(
+	'id'          => 'preheaderleft',
+	'name'        => __( 'PreHeaderLeft', 'genesischild' ),
+	'description' => __( 'This is the preheader left area', 'genesischild' ),
+	'before_widget' => '<div class="first one-half preheaderleft">',
+    'after_widget' => '</div>',
+	) );
+	genesis_register_sidebar( array(
+	'id'          => 'preheaderright',
+	'name'        => __( 'PreHeaderRight', 'genesischild' ),
+	'description' => __( 'This is the preheader right area', 'genesischild' ),
+	'before_widget' => '<div class="one-half preheaderright">',
+    'after_widget' => '</div>',
+	) );
+	genesis_register_sidebar( array(
+	'id'          => 'hero',
+	'name'        => __( 'Hero Home Page', 'genesischild' ),
+	'description' => __( 'This is the Hero Home Page area', 'genesischild' ),
+	'before_widget' => '<div class="wrap hero">',
+    'after_widget' => '</div>',
+	) );
+	genesis_register_sidebar( array(
+	'id'          => 'optin',
+	'name'        => __( 'Optin', 'genesischild' ),
+	'description' => __( 'This is the optin area', 'genesischild' ),
+	'before_widget' => '<div class="wrap optin">',
+    'after_widget' => '</div>',
+	) );
+	genesis_register_sidebar( array(
+	'id'          => 'beforecontent',
+	'name'        => __( 'Before Content', 'genesischild' ),
+	'description' => __( 'This is the before content area', 'genesischild' ),
+	'before_widget' => '<div class="beforecontent">',
+    'after_widget' => '</div>',
+	) );
+	genesis_register_sidebar( array(
+	'id'          => 'aftercontent',
+	'name'        => __( 'After Content', 'genesischild' ),
+	'description' => __( 'This is the before content area', 'genesischild' ),
+	'before_widget' => '<div class="aftercontent">',
+    'after_widget' => '</div>',
+	) );
+	genesis_register_sidebar( array(
+    'id' => 'footerwidgetheader',
+    'name' => __( 'Footer Widget Header', 'genesischild' ),
+    'description' => __( 'This is for the Footer Widget Headline', 'genesischild' ),
+    ) );
+	genesis_register_sidebar( array(
 	'id'          => 'footercontent',
 	'name'        => __( 'Footer', 'genesischild' ),
 	'description' => __( 'This is the general footer area', 'genesischild' ),
 	'before_widget' => '<div class="footercontent">',
     'after_widget' => '</div>',
 	) );
+	genesis_register_sidebar( array(
+	'id'          => 'postfooterleft',
+	'name'        => __( 'Post Footer Left', 'genesischild' ),
+	'description' => __( 'This is the post footer left area', 'genesischild' ),
+	'before_widget' => '<div class="first one-half postfooterleft">',
+    'after_widget' => '</div>',
+	) );
+	genesis_register_sidebar( array(
+	'id'          => 'postfooterright',
+	'name'        => __( 'Post Footer Right', 'genesischild' ),
+	'description' => __( 'This is the post footer right area', 'genesischild' ),
+	'before_widget' => '<div class="one-half postfooterright">',
+    'after_widget' => '</div>',
+	) );
+}
+//Position the PreHeader Area
+function genesischild_preheader_widget() {
+	echo '<div class="preheadercontainer"><div class="wrap">';
+    genesis_widget_area ('preheaderleft');
+	genesis_widget_area ('preheaderright');
+	echo '</div></div>';
+}
+//Position the Hero Area
+function genesischild_hero_widget() {
+    genesis_widget_area ('hero', array(
+        'before' => '<div class="herocontainer">',
+        'after' => '</div>',));
 }
 
+//Position the Optin Area
+function genesischild_optin_widget() {
+    genesis_widget_area ('optin', array(
+        'before' => '<div class="optincontainer">',
+        'after' => '</div>',));
+}
+//Position Footer Widget Header
+function genesischild_footerwidgetheader()  {
+    echo '<div class="footerwidgetheader-container"><div class="wrap">';
+    genesis_widget_area ('footerwidgetheader');
+    echo '</div></div>'; 
+}
 	
 //Position the Footer Area
 function genesischild_footer_widget() {
@@ -62,6 +163,25 @@ function genesischild_footer_widget() {
         'before' => '<div class="footercontainer">',
         'after' => '</div>',));
 }
+
+//Position the PostFooter Area
+function genesischild_postfooter_widget() {
+	echo '<div class="postfootercontainer"><div class="wrap">';
+    genesis_widget_area ('postfooterleft');
+	genesis_widget_area ('postfooterright');
+	echo '</div></div>';
+}
+
+//Position the Before Content Area
+function genesischild_beforecontent_widget() {
+    genesis_widget_area ('beforecontent');
+}
+
+//Position the After Content Area
+function genesischild_aftercontent_widget() {
+    genesis_widget_area ('aftercontent');
+}
+
 
 //Allow PHP to run in Widgets
 function execute_php_widgets($html){
